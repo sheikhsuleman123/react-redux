2725ea727c7cf9fa972d2e66007098be8acebdcb
sheikhsuleman123 <mssheikh333@gmail.com>
2020-04-05 11:26:17 +0500

    showing the state from reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import {connect} from 'react-redux';
+
+function App(props) {
+  console.log(props);
   return (
     <div className="App">
       <h1>Hallo from App</h1>
+      <h1>My name is {props.myname}</h1>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state)=> {
+  return {
+    myname:state.name
+  }
+}
+
+export default connect(mapStateToProps)(App);
